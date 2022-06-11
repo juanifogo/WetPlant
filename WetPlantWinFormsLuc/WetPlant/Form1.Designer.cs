47da9edc821ea272c4ac6d3736643c90653da91e
Luc-cr <72467699+Luc-cr@users.noreply.github.com>
2022-06-11 12:35:51 -0300

    addNewWindowsForm

---
+++ WetPlantWinFormsLuc/WetPlant/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WetPlant
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Text_1 = new System.Windows.Forms.Label();
+            this.Text_2 = new System.Windows.Forms.Label();
+            this.Text_3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Text_1
+            // 
+            this.Text_1.AutoSize = true;
+            this.Text_1.Location = new System.Drawing.Point(172, 193);
+            this.Text_1.Name = "Text_1";
+            this.Text_1.Size = new System.Drawing.Size(45, 13);
+            this.Text_1.TabIndex = 0;
+            this.Text_1.Text = "Label_1";
+            this.Text_1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Text_2
+            // 
+            this.Text_2.AutoSize = true;
+            this.Text_2.Location = new System.Drawing.Point(380, 193);
+            this.Text_2.Name = "Text_2";
+            this.Text_2.Size = new System.Drawing.Size(35, 13);
+            this.Text_2.TabIndex = 1;
+            this.Text_2.Text = "label1";
+            // 
+            // Text_3
+            // 
+            this.Text_3.AutoSize = true;
+            this.Text_3.Location = new System.Drawing.Point(538, 193);
+            this.Text_3.Name = "Text_3";
+            this.Text_3.Size = new System.Drawing.Size(35, 13);
+            this.Text_3.TabIndex = 2;
+            this.Text_3.Text = "label1";
+            this.Text_3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Text_3);
+            this.Controls.Add(this.Text_2);
+            this.Controls.Add(this.Text_1);
+            this.Name = "MainWindow";
+            this.Text = "WetPlant";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Text_1;
+        private System.Windows.Forms.Label Text_2;
+        private System.Windows.Forms.Label Text_3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WetPlantWinFormsLuc/WetPlant/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WetPlant
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load += new EventHandler(Form1_load);
+        }
+
+        // Handle: Cargar Form
+        private void Form1_load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Serial serial = new Serial();
+            serial.Config("COM4", 9600);
+            serial.SetTimeout(500, 500);
+            serial.Open();
+           
+
+            serial.Close();
+        }
+
+        private void asyncFunct()
+        {
+
+        }
+    }
+    public class Serial
+    {
+        static SerialPort serialport;
+        public Serial()
+        {
+            serialport = new SerialPort();
+        }
+
+        public void Config(string port, int baudrate)
+        {
+            serialport.PortName = port;
+            serialport.BaudRate = baudrate;
+            serialport.Parity = Parity.None;
+            serialport.StopBits = StopBits.One;
+            serialport.Handshake = Handshake.None;
+        }
+
+        public bool Open()
+        {
+            try
+            {
+                serialport.Open();
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void SetTimeout(int read, int write)
+        {
+            serialport.ReadTimeout = read;
+            serialport.WriteTimeout = write;
+        }
+
+        public void Close()
+        {
+            serialport.Close();
+        }
+
+        public string ReadData()
+        {
+            return serialport.ReadLine();
+        }
+
+        public void SendData(string data)
+        {
+            serialport.Write(data);
+        }
+    }
+}
