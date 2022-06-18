6e5854fb785332177ab68b02bd9749b557e855aa
Luc-cr <72467699+Luc-cr@users.noreply.github.com>
2022-06-18 10:44:50 -0300

    FuncionalidadTerminada

--- WetPlantWinFormsLuc/WetPlant/Form1.Designer.cs
+++ WetPlantWinFormsLuc/WetPlant/Form1.Designer.cs
@@ -28,73 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Text_1 = new System.Windows.Forms.Label();
-            this.Text_2 = new System.Windows.Forms.Label();
-            this.Text_3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // Text_1
-            // 
-            this.Text_1.AutoSize = true;
-            this.Text_1.Location = new System.Drawing.Point(172, 193);
-            this.Text_1.Name = "Text_1";
-            this.Text_1.Size = new System.Drawing.Size(45, 13);
-            this.Text_1.TabIndex = 0;
-            this.Text_1.Text = "Label_1";
-            this.Text_1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // Text_2
-            // 
-            this.Text_2.AutoSize = true;
-            this.Text_2.Location = new System.Drawing.Point(380, 193);
-            this.Text_2.Name = "Text_2";
-            this.Text_2.Size = new System.Drawing.Size(35, 13);
-            this.Text_2.TabIndex = 1;
-            this.Text_2.Text = "label1";
-            // 
-            // Text_3
-            // 
-            this.Text_3.AutoSize = true;
-            this.Text_3.Location = new System.Drawing.Point(538, 193);
-            this.Text_3.Name = "Text_3";
-            this.Text_3.Size = new System.Drawing.Size(35, 13);
-            this.Text_3.TabIndex = 2;
-            this.Text_3.Text = "label1";
-            this.Text_3.Click += new System.EventHandler(this.label2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(383, 306);
+            this.button1.Location = new System.Drawing.Point(348, 382);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Click";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Luz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Humedad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Temperatura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(463, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 700);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.Text_3);
-            this.Controls.Add(this.Text_2);
-            this.Controls.Add(this.Text_1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.Name = "MainWindow";
             this.Text = "WetPlant";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Text_1;
-        private System.Windows.Forms.Label Text_2;
-        private System.Windows.Forms.Label Text_3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WetPlantWinFormsLuc/WetPlant/Form1.cs
+++ WetPlantWinFormsLuc/WetPlant/Form1.cs
@@ -8,46 +8,105 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace WetPlant
 {
     public partial class MainWindow : Form
     {
+        private Serial serial = new Serial();
+        private Thread thread;
+
         public MainWindow()
         {
             InitializeComponent();
-            Load += new EventHandler(Form1_load);
-        }
-
-        // Handle: Cargar Form
-        private void Form1_load(object sender, EventArgs e)
-        {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Serial serial = new Serial();
-            serial.Config("COM4", 9600);
-            serial.SetTimeout(500, 500);
-            serial.Open();
-           
-
-            serial.Close();
+     
         }
 
-        private void asyncFunct()
+        public void UpdateData()
         {
+            while (true)
+            {
+                string[] fdata = { "", "", "" };
+                string data = serial.ReadData();
+                int j = 0;
 
+                for (int i = 0; i < data.Length; i++)
+                {
+                    fdata[j] += data[i].ToString();
+                    if (data[i].ToString() == " ")
+                    {
+                        j++;
+                    }
+                }
+                label1.Text = fdata[0].ToString();
+                label3.Text = fdata[1].ToString();
+                label4.Text = fdata[2].ToString();
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            serial.Config("COM4", 9600);
+            serial.SetTimeout(500, 500);
+            if(serial.Open() == false)
+            {
+                MessageBox.Show("Error al conectarse con el puerto serie!!", "Fatal_Error");
+                
+                Environment.Exit(1);
+            }
+            thread = new Thread(new ThreadStart(UpdateData));
+            thread.Start();
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread.Abort();
         }
     }
     public class Serial
     {
-        static SerialPort serialport;
+        SerialPort serialport;
         public Serial()
         {
             serialport = new SerialPort();
         }
+        /*
+        public string GetPort()
+        {
+            // Generamos un proceso Para obtener los coms
+            var proc = new Process { StartInfo = new ProcessStartInfo { 
+                FileName= @"C:\Windows\System32\mode.com", 
+                UseShellExecute = false, 
+                RedirectStandardOutput= true, 
+                CreateNoWindow=true 
+            } };
+            proc.Start();
+            proc.WaitForExit(4000);
+
+            var output = proc.StandardOutput.ReadToEnd();
+
+            List<string> comPortsBeingUsed = new List<string>();
+            Regex.Replace(output, @"(?xi) status [\s\w]+? (COM\d) \b ", regexCapture =>
+            {
+                comPortsBeingUsed.Add(regexCapture.Groups[1].Value);
+                return null;
+            });
+
+            foreach (var item in comPortsBeingUsed)
+            {
+                return item;
+            }
+            return "";
+        }
+        */
 
         public void Config(string port, int baudrate)
         {
